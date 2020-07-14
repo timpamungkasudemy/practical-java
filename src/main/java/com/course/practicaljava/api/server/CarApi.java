6fff42800acc0c73391dcde4732bc17f169abbc5
timpamungkas <timotius.pamungkas@gmail.com>
2020-07-15 06:52:04 +0700

    48 Customize HTTP Response

--- src/main/java/com/course/practicaljava/api/server/CarApi.java
+++ src/main/java/com/course/practicaljava/api/server/CarApi.java
@@ -1,10 +1,12 @@
 package com.course.practicaljava.api.server;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.course.practicaljava.api.response.ErrorResponse;
 import com.course.practicaljava.entity.Car;
 import com.course.practicaljava.repository.CarElasticRepository;
 import com.course.practicaljava.service.CarService;
@@ -94,10 +100,22 @@ public class CarApi {
 	}
 
 	@GetMapping(value = "/cars/{brand}/{color}")
-	public List<Car> findCarsByPath(@PathVariable String brand, @PathVariable String color,
+	public ResponseEntity<Object> findCarsByPath(@PathVariable String brand, @PathVariable String color,
 			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+		var headers = new HttpHeaders();
+		headers.add(HttpHeaders.SERVER, "Spring");
+		headers.add("X-Custom-Header", "Custom Response Header");
+
+		if (StringUtils.isNumeric(color)) {
+			var errorResponse = new ErrorResponse("Invalid color : " + color, LocalDateTime.now());
+
+			return new ResponseEntity<Object>(errorResponse, headers, HttpStatus.BAD_REQUEST);
+		}
+
 		var pageable = PageRequest.of(page, size);
-		return carRepository.findByBrandAndColor(brand, color, pageable).getContent();
+		var cars = carRepository.findByBrandAndColor(brand, color, pageable).getContent();
+
+		return ResponseEntity.ok().headers(headers).body(cars);
 	}
 
 	@GetMapping(value = "/cars")
