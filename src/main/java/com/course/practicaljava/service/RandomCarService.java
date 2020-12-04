71bcc45de80a94703083f896ade8b4a7cb6f0162
timpamungkas <timotius.pamungkas@gmail.com>
2020-12-04 08:04:27 +0700

    fixing car

--- src/main/java/com/course/practicaljava/service/RandomCarService.java
+++ src/main/java/com/course/practicaljava/service/RandomCarService.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.stereotype.Service;
 
 import com.course.practicaljava.entity.Car;
+import com.course.practicaljava.util.RandomDateUtil;
 
 @Service
 public class RandomCarService implements CarService {
@@ -15,7 +16,16 @@ public class RandomCarService implements CarService {
 		var color = COLORS.get(ThreadLocalRandom.current().nextInt(0, COLORS.size()));
 		var type = TYPES.get(ThreadLocalRandom.current().nextInt(0, TYPES.size()));
 
-		return new Car(brand, color, type);
+		var available = ThreadLocalRandom.current().nextBoolean();
+		var price = ThreadLocalRandom.current().nextInt(5000, 12001);
+		var firstReleaseDate = RandomDateUtil.generateRandomLocalDate();
+
+		var result = new Car(brand, color, type);
+		result.setAvailable(available);
+		result.setPrice(price);
+		result.setFirstReleaseDate(firstReleaseDate);
+
+		return result;
 	}
 
 }
